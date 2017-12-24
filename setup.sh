7b582e2b2d5574d0c2c590e4372bf0992e44a753
Alex Levin <alexlevinb@gmail.com>
2017-12-24 11:28:58 -0500

    dwm

--- setup.sh
+++ setup.sh
@@ -2,9 +2,6 @@
 
 cd ~
 git clone https://git.suckless.org/dwm
-chmod +x ~/github/dotfiles/dwm/run_mygtkmenu.sh
-chmod +x ~/github/dotfiles/dwm/dwm_status
-# cp ~/github/dotfiles/dwm/config.h ~/dwm/
 ln -s ~/github/dotfiles/dwm/config.h ~/dwm/config.h
 ln -s ~/github/dotfiles/.SciTEUser.properties ~/.SciTEUser.properties
 make -C ~/dwm
