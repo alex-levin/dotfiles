4a12fd2e277fe80d2df86f9d1ac02b1018e3a432
Alex Levin <alexlevinb@gmail.com>
2017-12-24 11:53:01 -0500

    dwm

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 cd ~
+chmod + x /home/alex/github/dotfiles/dwm/dwm_status.sh
 git clone https://git.suckless.org/dwm
+ln -s ~/github/dotfiles/.xinitrc ~/.xinitrc
 ln -s ~/github/dotfiles/dwm/config.h ~/dwm/config.h
 ln -s ~/github/dotfiles/.SciTEUser.properties ~/.SciTEUser.properties
 make -C ~/dwm
